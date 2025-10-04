897ad3f5b3c5be13c9f060ee3089cabe0fde7ce1
Abdulkadir-22 <yusuf22086@gmail.com>
2025-10-04 13:51:31 +0530

    Frontend: Navbar Created

--- Frontend/src/Routes.jsx
+++ Frontend/src/Routes.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom';
 import Signup from './pages/Signup';
 import Login from './pages/Login';
 import ExpenseSubmission from './pages/ExpenseSubmission';
+import ReceiptScanner from './components/ReceiptScanner'
 
 
 const AppRoutes = () => {
@@ -10,6 +11,8 @@ const AppRoutes = () => {
       <Route path="/signup" element={<Signup />} />
       <Route path="/login" element={<Login />} />
       <Route path="/submit" element={<ExpenseSubmission/>}/>
+      <Route path="/scanner" element={<ReceiptScanner/>}/>
+      <Route path="/" element={<ExpenseSubmission/>}/>
     </Routes>
   );
 };
