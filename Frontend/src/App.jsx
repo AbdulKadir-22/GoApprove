963ef0863309a6971d33f6e7f8aba6104a9becc2
Meet Vora <vorameet3719@gmail.com>
2025-10-04 15:22:49 +0530

    Frontend : Request card

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,9 +1,12 @@
 import AppRoutes from './Routes';
+import RequestCard from './components/Requestcard';
 
 function App() {
   return (
     <div>
       <AppRoutes />
+      <RequestCard />
+      
     </div>
   );
 }
